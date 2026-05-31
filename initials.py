1282f1d0f5a849029fb461713b401bd503c3f410
Donald-gctu <2425404837@live.gctu.edu.gh>
2026-05-31 14:01:58 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Donald Koffi Ange Akpagana
+# STUDENT ID: 2425404837
+# Email: 2425404837@live.gctu.edu.gh
+# Grader: Carolyn
+#
+# On my honor, Donald Koffi Ange Akpagana, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters
 
+    print()
+
+    print("...ADK")
+
+    print()
+
+    print("...AAAAAAAAAA...DDDDDDDDDD...K........K...****")
+    print("...A........A...D........D...K......K.....****")
+    print("...A........A...D........D...K....K.......****")
+    print("...A........A...D........D...K..K.........****")
+    print("...AAAAAAAAAA...D........D...KK...........****")
+    print("...A........A...D........D...K..K.........")
+    print("...A........A...D........D...K....K.......")
+    print("...A........A...D........D...K......K.....")
+    print("...A........A...D........D...K........K...")
+    print("...A........A...DDDDDDDDDD...K.........K...")
+
+    print()
 
 
 main()
